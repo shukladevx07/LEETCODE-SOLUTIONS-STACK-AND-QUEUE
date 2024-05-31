2fb250c759ba4648944857316a97f73165818ab7
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-05-31 23:57:46 +0530

    Time: 5 ms (20.29%), Space: 43.1 MB (79.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,33 +1,40 @@
+import java.util.Stack;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int l1 = nums1.length;
         int l2 = nums2.length;
-        int arr[] = new int[l1];
+        int[] result = new int[l1];
+        int[] nextGreater = new int[l2];
         
-        for(int i = 0; i < l1; i++) {
-            boolean found = false;
-            for(int j = 0; j < l2; j++) {
-                if(nums1[i] == nums2[j]) {
-                    found = true;
-                    // Look for the next greater element
-                    boolean greaterFound = false;
-                    for(int k = j + 1; k < l2; k++) {
-                        if(nums2[k] > nums2[j]) {
-                            arr[i] = nums2[k];
-                            greaterFound = true;
-                            break;
-                        }
-                    }
-                    if(!greaterFound) {
-                        arr[i] = -1;
-                    }
-                    break; // Break out of the inner loop once the element is found
+        // Initialize the next greater array with -1
+        for (int i = 0; i < l2; i++) {
+            nextGreater[i] = -1;
+        }
+        
+        // Stack to keep track of indices of elements for which we are finding the next greater
+        Stack<Integer> stack = new Stack<>();
+        
+        // Process nums2 to find the next greater element for each element
+        for (int i = 0; i < l2; i++) {
+            // While stack is not empty and the current element is greater than the element at the index stored in the stack
+            while (!stack.isEmpty() && nums2[i] > nums2[stack.peek()]) {
+                nextGreater[stack.pop()] = nums2[i];
+            }
+            // Push the current index onto the stack
+            stack.push(i);
+        }
+        
+        // Map results to nums1
+        for (int i = 0; i < l1; i++) {
+            for (int j = 0; j < l2; j++) {
+                if (nums1[i] == nums2[j]) {
+                    result[i] = nextGreater[j];
+                    break;
                 }
             }
-            if(!found) {
-                arr[i] = -1; // In case nums1[i] is not found in nums2, though it's stated to be a subset
-            }
         }
-        return arr;
+        
+        return result;
     }
 }
