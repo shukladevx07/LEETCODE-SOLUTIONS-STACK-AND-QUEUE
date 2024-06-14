ea4ee7199303e2b966af2e270731db093e5d6875
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-06-15 02:00:54 +0530

    Time: 2 ms (88.45%), Space: 55.2 MB (71.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
-        
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
-        }
-        
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
-        }
-        return ans;
+    public int[] productExceptSelf(int[] arr) {
+         int[] left= new int[arr.length];
+            int[] right=new int[arr.length];
+            left[0]=1;
+            for(int i=1; i< arr.length; i++)
+            {
+                left[i]=left[i-1]*arr[i-1];
+            }
+            right[arr.length-1]=1;
+            for(int i=arr.length-2; i>=0;i--)
+            {
+                right[i]=right[i+1]*arr[i+1];
+            }
+            for(int i=0;i<arr.length;i++)
+            {
+                arr[i]=left[i]*right[i];
+            }
+        return arr;
     }
 }
