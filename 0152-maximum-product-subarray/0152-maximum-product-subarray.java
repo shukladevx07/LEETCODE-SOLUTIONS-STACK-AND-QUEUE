b75eac9318e7d3e46b56c43636dfed670b7650f9
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-06-15 02:32:10 +0530

    Time: 2 ms (59.23%), Space: 44.5 MB (86.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
         int maxProduct = nums[0];
         int currentMax = nums[0];
         int currentMin = nums[0];
@@ -12,9 +16,11 @@ class Solution {
                 currentMin = temp;
             }
             
+            // Calculate the maximum and minimum product ending at the current position
             currentMax = Math.max(nums[i], currentMax * nums[i]);
             currentMin = Math.min(nums[i], currentMin * nums[i]);
             
+            // Update the overall maximum product
             maxProduct = Math.max(maxProduct, currentMax);
         }
         
