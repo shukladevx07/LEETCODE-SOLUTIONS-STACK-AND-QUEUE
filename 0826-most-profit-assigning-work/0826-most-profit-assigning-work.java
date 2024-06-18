1bac6e2f3b2bd75e93d440e63ae2fc1c94adb579
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-06-19 01:06:29 +0530

    Time: 23 ms (45.27%), Space: 45.3 MB (78.4%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,18 +1,32 @@
+import java.util.Arrays;
+
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        int n1 = difficulty.length;
-        int n2 = worker.length;
-        int sum = 0;
+        int n = difficulty.length;
 
-        for (int i = 0; i < n2; i++) {
-            int largest = 0;
-            for (int j = 0; j < n1; j++) {
-                if (worker[i] >= difficulty[j]) {
-                    largest = Math.max(largest, profit[j]);
-                }
-            }
-            sum += largest;
+        // Create an array of jobs with difficulty and profit
+        int[][] jobs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
         }
-        return sum;
+
+        // Sort jobs by difficulty
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        // Sort workers by their ability
+        Arrays.sort(worker);
+
+        int maxProfit = 0, bestProfit = 0, i = 0;
+        for (int ability : worker) {
+            // While the worker can complete the job, update the bestProfit
+            while (i < n && ability >= jobs[i][0]) {
+                bestProfit = Math.max(bestProfit, jobs[i][1]);
+                i++;
+            }
+            // Add the best profit the worker can get to the total profit
+            maxProfit += bestProfit;
+        }
+
+        return maxProfit;
     }
 }
