575b2ff602a92fb8aa9ad4f56b9ab3177925c163
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-06-20 12:04:48 +0530

    Time: 0 ms (100%), Space: 41.7 MB (10.88%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,43 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        TreeNode curr = root;
-
-        while(curr != null) {
-            if(curr.left == null) {
-                ans.add(curr.val); // Current node ka value add karo
-                curr = curr.right; // Right child pe move karo
+        List<Integer> inorder = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while (true) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
             } else {
-                TreeNode prev = curr.left;
-                // Find rightmost node in left subtree
-                while(prev.right != null && prev.right != curr) {
-                    prev = prev.right;
+                if (stack.isEmpty()) {
+                    break;
                 }
-                if(prev.right == null) {
-                    prev.right = curr; // Make temporary link to root
-                    curr = curr.left; // Left child pe move karo
-                } else {
-                    prev.right = null; // Remove temporary link
-                    ans.add(curr.val); // Current node ka value add karo
-                    curr = curr.right; // Right child pe move karo
-                }
+                node = stack.pop();
+                inorder.add(node.val);
+                node = node.right;
             }
         }
-        return ans; // Inorder traversal ka result return karo
+        return inorder;
     }
 }
