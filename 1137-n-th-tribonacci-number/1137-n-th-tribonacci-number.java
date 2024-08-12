0b39ed7d4961091b47d7159628488dff6c707680
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-08-12 16:53:29 +0530

    Time: 0 ms (100%), Space: 40.2 MB (62%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,17 +1,18 @@
+import java.util.Arrays;
 class Solution {
     public int tribonacci(int n) {
-         if (n < 3) {
-            return n == 0 ? 0 : 1;
-        }
-
-        int a = 0, b = 1, c = 1;
-
-        for (int i = 2; i < n; i++) {
-            int temp = a + b + c;
-            a = b;
-            b = c;
-            c = temp;
-        }
-        return c;
+         if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        return tribonacciHelper(n, dp);
+    }
+    private int tribonacciHelper(int n, int[] dp) {
+        if (dp[n] != -1) return dp[n];
+        dp[n] = tribonacciHelper(n - 1, dp) + tribonacciHelper(n - 2, dp) + tribonacciHelper(n - 3, dp);
+        return dp[n];
     }
 }
