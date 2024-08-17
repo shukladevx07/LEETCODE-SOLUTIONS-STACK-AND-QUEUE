a6f46c6f79234caf11f73033dd67ab45540a5778
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-08-17 11:56:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        return helper(n-1,amount,coins,dp);
+        
+    }
+    private int helper(int n,int amt,int[] coins,int[][] dp){
+        if(amt==0){
+            return 1;
+        }
+        if (n < 0 || amt < 0) {
+            return 0;
+        }
+        if(dp[n][amt]!=0){
+            return dp[n][amt];
+        }
+        int nottake=helper(n-1,amt,coins,dp);
+        int take=0;
+        if(amt>=coins[n]){
+            take=helper(n,amt-coins[n],coins,dp);
+        }
+        return dp[n][amt]=take+nottake;
+    }
+}
