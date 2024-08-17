6c4c859e697abcd3af3b12776e87ee99f8961a90
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-08-17 10:22:41 +0530

    Time: 6 ms (74.55%), Space: 45 MB (17.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,34 @@
 class Solution {
-
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, nums.length - 1, target);
+        int sum=0;
+        for (int num : nums) sum += num;
+        if (Math.abs(target) > sum) return 0 ;
+
+        Integer[][] dp = new Integer[nums.length][2 * sum + 1];
+
+        return helper(nums, nums.length - 1, target + sum, dp, sum);
     }
 
-    private int helper(int[] nums, int n, int target) {
+    private int helper(int[] nums, int n, int target, Integer[][] dp, int sum) {
+        if (target < 0 || target >= dp[0].length) {
+            return 0;
+        }
+
         if (n == 0) {
             int ways=0;
-            if (nums[0] == target) ways += 1;
-            if (nums[0] == -target) ways += 1;
+            if (nums[0] == target - sum) ways += 1;
+            if (nums[0] == -target + sum) ways += 1;
             return ways;
         }
-        int add= helper(nums, n - 1, target - nums[n]);
-        int subtract= helper(nums, n - 1, target + nums[n]);
-        return add + subtract;
+
+        if (dp[n][target] != null) {
+            return dp[n][target];
+        }
+
+        int add= helper(nums, n - 1, target - nums[n], dp, sum);
+        int subtract= helper(nums, n - 1, target + nums[n], dp, sum);
+
+        dp[n][target] = add + subtract;
+        return dp[n][target];
     }
 }
