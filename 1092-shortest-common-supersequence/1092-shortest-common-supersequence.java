4fb8af513c85d62a4c4a2cab4fcd81cd0c589ad3
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-08-20 16:41:57 +0530

    Time: 21 ms (80.62%), Space: 50.6 MB (19.7%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -3,34 +3,29 @@ class Solution {
         int n = str1.length();
         int m = str2.length();
 
-        // Only keep the current and previous row in memory
-        int[] prev = new int[m + 1];
-        int[] curr = new int[m + 1];
+        int[][] dp = new int[n + 1][m + 1];
 
-        // Fill the DP array
+        // Building the dp table
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    curr[j] = 1 + prev[j - 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    curr[j] = Math.max(prev[j], curr[j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
-            // Swap current and previous row references
-            int[] temp = prev;
-            prev = curr;
-            curr = temp;
         }
 
         // Reconstruct the shortest common supersequence
         StringBuilder ans = new StringBuilder();
         int i = n, j = m;
+
         while (i > 0 && j > 0) {
             if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                 ans.append(str1.charAt(i - 1));
                 i--;
                 j--;
-            } else if (prev[j] > curr[j - 1]) {
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
                 ans.append(str1.charAt(i - 1));
                 i--;
             } else {
