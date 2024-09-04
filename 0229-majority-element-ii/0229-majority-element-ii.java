9c09f1096bb161937d6598de9282e8c9e1bd3f0e
Vibhanshu Shukla <122717130+shukladevx07@users.noreply.github.com>
2024-09-04 17:27:33 +0530

    Time: 2 ms (99.47%), Space: 48.3 MB (12.82%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,27 +3,45 @@ import java.util.*;
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int n1 = n / 3;
-        Arrays.sort(nums);
-        int c = 1;  // Start with 1 since first element is counted
-        List<Integer> list = new ArrayList<>();
+        if (n == 0) return new ArrayList<>();
         
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                c++;
+        // Step 1: Find potential candidates
+        int candidate1 = 0, candidate2 = 1;  // Initialize to different values
+        int count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
             } else {
-                if (c > n1) {
-                    list.add(nums[i - 1]);
-                }
-                c = 1;  // Reset count for the new number
+                count1--;
+                count2--;
             }
         }
         
-        // Check for the last element group
-        if (c > n1) {
-            list.add(nums[n - 1]);
+        // Step 2: Verify the candidates
+        count1 = 0;
+        count2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
         }
         
-        return list;
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+        
+        return result;
     }
 }
